3c4f1b5b10cc49cbb6c9b29e851cf39f88f7465b
Sairam8104 <sairamgoud.devarakadra@gmail.com>
2024-04-29 12:06:16 +0530

    commit message

---
+++ StudentForms/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Header from './components/Header'
+import StudentTable from './components/StudentTable'
+import TableItem from './components/TableItem'
+import StudentForm from './components/StudentForm';
+const App = () => (
+  <Router>
+    <Header/>
+  <Routes>
+    <Route exact path="/" element={<StudentTable />} />
+    <Route exact path="/studentform/:StudentId" element={<StudentForm />} />
+    <Route exact path="/studentform" element={<StudentForm />} />  {/* Define your root route here */}
+    <Route exact path="/tableItem" component={TableItem} />
+   
+  </Routes>
+</Router>
+)
+
+export default App
